ad1a4f8a2aadc839e40d26c7fe75a4e618939ff5
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-18 20:25:07 +0200

    fix: added linuxbrew wrapper

--- CI/install.sh
+++ CI/install.sh
@@ -1,15 +1,16 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+sudo apt install linuxbrew-wrapper
 brew install swiftlint
 brew install libressl
 brew install vapor/tap/vapor
+brew install postgresql
 
 swift --version
 swift package tools-version --set-current
 swift package resolve
 swift package clean
 
-rm -rf /usr/local/var/postgres
-initdb /usr/local/var/postgres -E utf8
-pg_ctl -D /usr/local/var/postgres start && sleep 3 || true
-sudo -u travis createuser -s -p 5432 postgres
-psql -U postgres -c 'create database test;'
+#rm -rf /usr/local/var/postgres
+#initdb /usr/local/var/postgres -E utf8
+#pg_ctl -D /usr/local/var/postgres start && sleep 3 || true
+#sudo -u travis createuser -s -p 5432 postgres
+#psql -U postgres -c 'create database test;'
