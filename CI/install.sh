bd935234548af30f8e4adce7ee353a85eacd0e81
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-18 21:16:50 +0200

    fix: installation changes

--- CI/install.sh
+++ CI/install.sh
@@ -1,8 +1,9 @@
-git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
 brew install swiftlint
 brew install libressl
@@ -12,11 +13,11 @@ brew install postgresql
 sudo apt-get install clang
 sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev
 
-wget https://swift.org/builds/swift-$SWIFT-VERSION-release/ubuntu1804/swift-$SWIFT-VERSION-RELEASE/swift-$SWIFT-VERSION-RELEASE-ubuntu18.04.tar.gz
+wget https://swift.org/builds/swift-$SWIFT_VERSION-release/ubuntu1804/swift-$SWIFT_VERSION-RELEASE/swift-$SWIFT_VERSION-RELEASE-ubuntu18.04.tar.gz
 tar xzf swift-$SWIFT_VERSION-RELEASE-ubuntu18.04.tar.gz
 sudo mv swift-$SWIFT_VERSION-RELEASE-ubuntu18.04 /usr/share/swift
-echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
-source  ~/.bashrc
+
+export PATH=/usr/share/swift/usr/bin:$PATH
 
 swift --version
 swift package tools-version --set-current
