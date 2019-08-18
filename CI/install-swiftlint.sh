32b64c1569c72097c1b114cbac830afa2a4c904a
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-19 00:46:42 +0200

    fix: pipeline changees

--- CI/install-swiftlint.sh
+++ CI/install-swiftlint.sh
@@ -6,9 +6,3 @@ swift build -c release --static-swift-stdlib
 mv .build/x86_64-unknown-linux/release/swiftlint /usr/local/bin/
 cd ..
 rm -rf SwiftLint
-
-swift package resolve
-swift package clean
-
-export DATABASE_DB=postgres
-export DATABASE_USER=postgres
