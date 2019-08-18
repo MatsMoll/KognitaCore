63d11e39faab7a18f903a67c11433b19d287d534
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-18 23:03:15 +0200

    fix: reverted back to bitbucket

--- CI/install.sh
+++ CI/install.sh
@@ -1,14 +1,9 @@
-brew install swiftlint
-brew install libressl
+git clone https://github.com/realm/SwiftLint.git
+cd SwiftLint
+swift build -c release --static-swift-stdlib
+mv .build/x86_64-unknown-linux/release/swiftlint /usr/local/bin/
+cd ..
+rm -rf SwiftLint
 
-swift --version
-swift package tools-version --set-current
-swift --version
 swift package resolve
 swift package clean
-
-rm -rf /usr/local/var/postgres
-initdb /usr/local/var/postgres -E utf8
-pg_ctl -D /usr/local/var/postgres start && sleep 3 || true
-sudo -u travis createuser -s -p 5432 postgres
-psql -U postgres -c 'create database test;'
