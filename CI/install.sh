a28782dcd57cd635ee3b02338c8a49fc9934bd3d
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-18 20:37:57 +0200

    fix: changed install steeps

--- CI/install.sh
+++ CI/install.sh
@@ -1,8 +1,18 @@
 sudo apt install linuxbrew-wrapper
+
 brew install swiftlint
 brew install libressl
 brew install vapor/tap/vapor
 brew install postgresql
+
+sudo apt-get install clang
+sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev
+
+wget https://swift.org/builds/swift-$SWIFT-VERSION-release/ubuntu1804/swift-$SWIFT-VERSION-RELEASE/swift-$SWIFT-VERSION-RELEASE-ubuntu18.04.tar.gz
+tar xzf swift-$SWIFT_VERSION-RELEASE-ubuntu18.04.tar.gz
+sudo mv swift-$SWIFT_VERSION-RELEASE-ubuntu18.04 /usr/share/swift
+echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
+source  ~/.bashrc
 
 swift --version
 swift package tools-version --set-current
