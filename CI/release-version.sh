7ceffb026217fa8ca70781a1b8b796b3de9bf623
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-19 10:34:29 +0200

    fix: release script

--- CI/release-version.sh
+++ CI/release-version.sh
@@ -11,11 +11,9 @@ MAJOR_VERSION=${VERSION_BITS[0]:-1}
 MINOR_VERSION=${VERSION_BITS[1]:-0}
 BUILD_VERSION=${VERSION_BITS[2]:-(-1)}
 
-echo "Updating $VERSION to $NEW_TAG"
-
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
-GIT_MESSAGE=`git show $GIT_COMMIT`
+GIT_MESSAGE="$(git log --format=%B -n 1 $GIT_COMMIT)"
 NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
@@ -24,8 +22,11 @@ if [ -z "$NEEDS_TAG" ]; then
 # Bumping version
 if [[ $GIT_MESSAGE == *"-MAJOR-"* ]]; then
 MAJOR_VERSION=$((MAJOR_VERSION+1))
+MINOR_VERSION=0
+BUILD_VERSION=0
 elif [[ $GIT_MESSAGE == *"-MINOR-"* ]]; then
 MINOR_VERSION=$((MINOR_VERSION+1))
+BUILD_VERSION=0
 else
 BUILD_VERSION=$((BUILD_VERSION+1))
 fi
@@ -33,10 +34,11 @@ fi
 #create new tag
 NEW_TAG="$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION"
 
+echo "Updating $VERSION to $NEW_TAG"
 echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
 
 git tag $NEW_TAG
-#git push origin $NEW_TAG
+git push origin $NEW_TAG
 else
 echo "Already a tag on this commit"
 exit 1
