c2693690671bf7a001593160f67e382b49dd4aab
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-19 09:37:36 +0200

    Changed release-version.sh

--- CI/release-version.sh
+++ CI/release-version.sh
@@ -11,9 +11,6 @@ MAJOR_VERSION=${VERSION_BITS[0]:-1}
 MINOR_VERSION=${VERSION_BITS[1]:-0}
 BUILD_VERSION=${VERSION_BITS[2]:-(-1)}
 
-#create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-
 echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
@@ -23,14 +20,21 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
 if [ -z "$NEEDS_TAG" ]; then
-if [[ $GIT_MESSAGE == *"!MAJOR!"* ]]; then
+
+# Bumping version
+if [[ $GIT_MESSAGE == *"-MAJOR-"* ]]; then
 MAJOR_VERSION=$((MAJOR_VERSION+1))
-elif [[ $GIT_MESSAGE == *"!MINOR!"* ]]; then
+elif [[ $GIT_MESSAGE == *"-MINOR-"* ]]; then
 MINOR_VERSION=$((MINOR_VERSION+1))
 else
 BUILD_VERSION=$((BUILD_VERSION+1))
 fi
+
+#create new tag
+NEW_TAG="$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION"
+
 echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+
 git tag $NEW_TAG
 #git push origin $NEW_TAG
 else
