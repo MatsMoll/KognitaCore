eceda00ed4a200c302acc12658801903cc55008d
Mats Eikeland Mollestad <mem@mollestad.no>
2019-08-19 09:32:48 +0200

    fix: removed files

--- CI/release-version.sh
+++ CI/release-version.sh
@@ -7,10 +7,9 @@ VERSION=`git describe --abbrev=0 --tags`
 VERSION_BITS=(${VERSION//./ })
 
 #get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]:-1}
-VNUM2=${VERSION_BITS[1]:-0}
-VNUM3=${VERSION_BITS[2]:-(-1)}
-VNUM3=$((VNUM3+1))
+MAJOR_VERSION=${VERSION_BITS[0]:-1}
+MINOR_VERSION=${VERSION_BITS[1]:-0}
+BUILD_VERSION=${VERSION_BITS[2]:-(-1)}
 
 #create new tag
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
@@ -19,13 +18,21 @@ echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
+GIT_MESSAGE=`git show $GIT_COMMIT`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
 if [ -z "$NEEDS_TAG" ]; then
+if [[ $GIT_MESSAGE == *"!MAJOR!"* ]]; then
+MAJOR_VERSION=$((MAJOR_VERSION+1))
+elif [[ $GIT_MESSAGE == *"!MINOR!"* ]]; then
+MINOR_VERSION=$((MINOR_VERSION+1))
+else
+BUILD_VERSION=$((BUILD_VERSION+1))
+fi
 echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
 git tag $NEW_TAG
-git push origin $NEW_TAG
+#git push origin $NEW_TAG
 else
 echo "Already a tag on this commit"
 exit 1
