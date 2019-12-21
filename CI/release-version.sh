af57b713c1493f8a1b7c4602689e802c5c915667
Mats Eikeland Mollestad <mem@mollestad.no>
2019-12-21 14:59:19 +0100

    Added finch config and added finch into release-version script

--- CI/release-version.sh
+++ CI/release-version.sh
@@ -65,14 +65,17 @@ function generate_new_release_data {
     fi
 
     #create new tag
-    NEXT_VERSION="$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION"
+    NEXT_VERSION="$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION-beta"
+
+    git tag $NEXT_VERSION
+    CHANGE_LOG="$(finch compare --release-manager=mem@mollestad.no --project-dir="../" --config="./finch-config.yml")"
 
     cat << EOF > new_release_data
 {
   "tag_name": "${NEXT_VERSION}",
   "target_commitish": "${GITHUB_SHA}",
   "name": "${NEXT_VERSION}",
-  "body": "Version ${NEXT_VERSION}",
+  "body": "${CHANGE_LOG}",
   "draft": false,
   "prerelease": false
 }
