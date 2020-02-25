7c13712a273fe24c00095547d8c98dbf6945e365
eskildbr <eskildbr@stud.ntnu.no>
2020-02-25 13:06:30 +0100

    [Feature] Added repositories for discussion on a task

--- Sources/KognitaCore/Task/Discussion/Repository/TaskDiscussionRepositoring.swift
+++ Sources/KognitaCore/Task/Discussion/Repository/TaskDiscussionRepositoring.swift
@@ -1,8 +1,14 @@
-//
-//  TaskDiscussionRepositoring.swift
-//  KognitaCore
-//
-//  Created by Eskild Brobak on 25/02/2020.
-//
+import Vapor
 
-import Foundation
+public protocol TaskDiscussionRepositoring:
+    CreateModelRepository,
+    UpdateModelRepository
+    where
+    Model           == TaskDiscussion,
+    CreateData      == TaskDiscussion.Create.Data,
+    CreateResponse  == TaskDiscussion.Create.Response,
+    UpdateData      == TaskDiscussion.Update.Data,
+    UpdateResponse  == TaskDiscussion.Create.Response
+{
+    static func respond(with response: TaskDiscussion.Pivot.Response.Create.Data, by user: User, on conn: DatabaseConnectable) throws -> EventLoopFuture<Void>
+}
--- Sources/KognitaCore/Task/Discussion/Repository/TaskDiscussionRepository.swift
+++ Sources/KognitaCore/Task/Discussion/Repository/TaskDiscussionRepository.swift
@@ -1,8 +1,33 @@
-//
-//  TaskDiscussionRepository.swift
-//  KognitaCore
-//
-//  Created by Eskild Brobak on 25/02/2020.
-//
+import Vapor
 
-import Foundation
+
+extension TaskDiscussion {
+    public class DatabaseRepository: TaskDiscussionRepositoring {
+
+        public static func create(from content: TaskDiscussion.Create.Data, by user: User?, on conn: DatabaseConnectable) throws -> EventLoopFuture<TaskDiscussion.Create.Response> {
+
+            guard let user = user else {
+                throw Abort(.unauthorized)
+            }
+
+            return try TaskDiscussion(data: content, userID: user.requireID())
+                .create(on: conn)
+                .transform(to: .init())
+        }
+
+        public static func update(model: TaskDiscussion, to data: TaskDiscussion.Update.Data, by user: User, on conn: DatabaseConnectable) throws -> EventLoopFuture<TaskDiscussion.Create.Response> {
+
+            guard user.id == model.userID else {
+                throw Abort(.forbidden)
+            }
+            model.update(with: data)
+            return model.save(on: conn)
+                .transform(to: .init())
+        }
+
+        public static func respond(with response: TaskDiscussion.Pivot.Response.Create.Data, by user: User, on conn: DatabaseConnectable) throws -> EventLoopFuture<Void> {
+
+            return try TaskDiscussion.DatabaseRepository.respond(with: response, by: user, on: conn)
+        }
+    }
+}
