1bca70448a9d31fde241683a249061160dd55f5c
Mats Eikeland Mollestad <mem@mollestad.no>
2020-03-15 11:33:10 +0100

    [TaskSolution] Multiple line solution test

--- Sources/KognitaCore/String+clean.swift
+++ Sources/KognitaCore/String+clean.swift
@@ -6,9 +6,16 @@ public enum CleaningError: Error {
 
 extension String {
     func cleanXSS(whitelist: Whitelist) throws -> String {
-        guard let string = try SwiftSoup.clean(self, whitelist) else {
+
+        // This is done since the new line will be removed when cleaning a html document
+        let noNewLine = self.split(separator: "\n").reduce("") { $0 + "<p>" + $1 + "</p>" }
+
+        guard let string = try SwiftSoup.clean(noNewLine, whitelist) else {
             throw CleaningError.noStringLeft
         }
         return string
+            .replacingOccurrences(of: "</p>\n<p>", with: "\n\n")
+            .replacingOccurrences(of: "</p>", with: "")
+            .replacingOccurrences(of: "<p>", with: "")
     }
 }
