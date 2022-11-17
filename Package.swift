d86612ca058387380c46c7950e0a6ddb3eabe881
Mats E. Mollestad <mats@mollestad.no>
2022-11-17 21:42:40 +0100

    fix: package file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import Foundation
 
 var dependencies: [Package.Dependency] = [
     // 💧 A server-side Swift web framework.
-    .package(name: "vapor", url: "https://github.com/vapor/vapor.git", from: "4.29.0"),
+    .package(name: "vapor", url: "https://github.com/vapor/vapor.git", from: "4.67.3"),
 
     // 🐘 Non-blocking, event-driven Swift client for PostgreSQL.
     .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.1.1"),
@@ -44,7 +44,7 @@ default:
 let package = Package(
     name: "KognitaCore",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
